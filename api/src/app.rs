6bfb7adc6bd338715b2adfe6475fca43a6ce604b
Buck Brady <bbrady145@gmail.com>
2024-09-29 00:56:17 -0600

    add rust test workflow

--- api/src/app.rs
+++ api/src/app.rs
@@ -16,7 +16,7 @@ use sea_orm::DatabaseConnection;
 
 use crate::{
     controllers,
-    models::_entities::{notes, users},
+    models::_entities::{users},
     tasks,
     workers::downloader::DownloadWorker,
 };
@@ -45,7 +45,6 @@ impl Hooks for App {
     fn routes(_ctx: &AppContext) -> AppRoutes {
         AppRoutes::with_default_routes()
             .prefix("/api")
-            .add_route(controllers::notes::routes())
             .add_route(controllers::auth::routes())
             .add_route(controllers::user::routes())
     }
@@ -60,13 +59,11 @@ impl Hooks for App {
 
     async fn truncate(db: &DatabaseConnection) -> Result<()> {
         truncate_table(db, users::Entity).await?;
-        truncate_table(db, notes::Entity).await?;
         Ok(())
     }
 
     async fn seed(db: &DatabaseConnection, base: &Path) -> Result<()> {
         db::seed::<users::ActiveModel>(db, &base.join("users.yaml").display().to_string()).await?;
-        db::seed::<notes::ActiveModel>(db, &base.join("notes.yaml").display().to_string()).await?;
         Ok(())
     }
 }
--- api/src/controllers/mod.rs
+++ api/src/controllers/mod.rs
@@ -1,3 +1,2 @@
 pub mod auth;
-pub mod notes;
 pub mod user;
